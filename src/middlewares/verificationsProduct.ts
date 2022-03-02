d83f4ecedbb4a91a4c8112b8c4f0c958f66beee7
Amanda Zotelli <amzotelli@gmail.com>
2022-03-02 17:28:34 -0300

    cria as verificacoes de products

---
+++ src/middlewares/verificationsProduct.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+
+const verifyName = (req: Request, res: Response, next: NextFunction) => {
+  const { name } = req.body;
+  if (!name) return res.status(400).json({ error: 'Name is required' });
+  if (typeof (name) !== 'string') {
+    return res.status(422).json({ error: 'Name must be a string' });
+  }
+  if (name.length <= 2) {
+    return res.status(422).json({ error: 'Name must be longer than 2 characters' });
+  }
+  next();
+};
+
+const verifyAmount = (req: Request, res: Response, next: NextFunction) => {
+  const { amount } = req.body;
+  if (!amount) return res.status(400).json({ error: 'Amount is required' });
+  if (typeof (amount) !== 'string') {
+    return res.status(422).json({ error: 'Amount must be a string' });
+  }
+  if (amount.length <= 2) {
+    return res.status(422).json({ error: 'Name must be longer than 2 characters' });
+  }
+  next();
+};
+
+export default {
+  verifyName,
+  verifyAmount,
+};
